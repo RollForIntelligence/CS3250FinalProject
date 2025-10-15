41f79fb74afb2d884f648d59415fbe68ef6e954f
RollForIntelligence <jameschecketts@mail.weber.edu>
2025-10-15 10:19:02 -0600

    Set up infrastructure for UI Displays

---
+++ FinalProject/src/UIDisplayPane.java
@@ -0,0 +1,15 @@
+import javafx.scene.layout.GridPane;
+
+public class UIDisplayPane extends GridPane {
+	
+	
+	public UIDisplayPane(Inventory inventory) {
+		// TODO: set up a grid of tiles for the items
+	}
+	
+	public UIDisplayPane(String message) {
+		// TODO: set the background color to black
+		// TODO: set the text color to red
+		// TODO: display the message at the center of the screen in large block text
+	}
+}
